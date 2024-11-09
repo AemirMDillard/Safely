63ce055e03c7c857a2155947f85a273a743382ae
Amir Dillard <dillard24a@ncssm.edu>
2024-11-09 13:38:50 -0500

    initial commit

---
+++ backend/suggestions.py
@@ -0,0 +1,10 @@
+
+
+def find_nearest_clinics(location):
+    return locations
+
+def find_birth_control_nearby(location):
+    return stores
+
+def find_contraceptives(location):
+    return stores
