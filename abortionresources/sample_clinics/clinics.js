c742343623fcafa1d3322663a604b5836d18f1b0
Amir Dillard <dillard24a@ncssm.edu>
2024-11-09 16:42:25 -0500

    committing header and code

---
+++ abortionresources/sample_clinics/clinics.js
@@ -0,0 +1,24 @@
+const clinics = [
+    {
+      image: "las-cruces.jpg",
+      name: "Las Cruces Health Center",
+      address: "2525 S. Telshor Suite 200 Bldg #15 Las Cruces, NM 88011",
+      times: 1090,
+      near_state: "Texas"
+    }
+    {
+        image: "las-cruces.jpg",
+        name: "Las Cruces Health Center",
+        address: "2525 S. Telshor Suite 200 Bldg #15 Las Cruces, NM 88011",
+        times: 1090,
+        near_state: "Texas"
+      }
+      {
+        image: "las-cruces.jpg",
+        name: "Las Cruces Health Center",
+        address: "2525 S. Telshor Suite 200 Bldg #15 Las Cruces, NM 88011",
+        times: 1090,
+        near_state: "Texas"
+      }
+
+];
